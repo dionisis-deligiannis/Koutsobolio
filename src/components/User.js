d470b5c556599db19213bb8152b314897bf1b7e7
dionisis-deligiannis <delisdionysios@gmail.com>
2022-07-26 21:11:39 +0300

    fix bugs with last msg

--- src/components/User.js
+++ src/components/User.js
@@ -1,18 +1,54 @@
-import React from 'react'
-import Img from "../images/user-profile-icon.jpg"
+import React, { useEffect, useState } from "react";
+import Img from "../images/user-profile-icon.jpg";
+import { onSnapshot, doc } from "firebase/firestore";
+import { db } from "../firebase";
 
-function User({user, selectUser}) {
+function User({ user1, user, selectUser, chat }) {
+  const user2 = user?.uid;
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+    let unsub = onSnapshot(doc(db, "lastMsg", id), (doc) => {
+      setData(doc.data());
+    });
+    return () => unsub();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div className='user_wrapper' onClick={()=>selectUser(user)}>
+    <>
+      <div
+        className={`user_wrapper ${chat.name === user.name && "selected_user"}`}
+        onClick={() => selectUser(user)}
+      >
         <div className="user_info">
-            <div className="user_detail">
-                <img src={user.avatar || Img} alt="avatar" className='avatar' />
-                <h4>{user.name}</h4>
-            </div>
-            <div className={`user_status ${user.isOnline ? "online" : "offline"}`}></div>
+          <div className="user_detail">
+            <img src={user.avatar || Img} alt="avatar" className="avatar" />
+            <h4>{user.name}</h4>
+          </div>
+          <div
+            className={`user_status ${user.isOnline ? "online" : "offline"}`}
+          ></div>
         </div>
-    </div>
-  )
+        {data && (
+          <p className="truncate">
+            {data?.from !== user1 && data?.unread && (
+              <small className="unread">New</small>
+            )}
+            <strong>{data.from === user1 ? "Me:" : null}</strong>
+            {data.text}
+          </p>
+        )}
+      </div>
+      <div onClick={() => selectUser(user)} className="sm_container">
+        <img
+          src={user.avatar || Img}
+          alt="avatar"
+          className="avatar sm_screen"
+        />
+      </div>
+    </>
+  );
 }
 
-export default User
+export default User;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,80 +3,115 @@ import User from "../components/User";
 import MessageForm from "../components/MessageForm";
 import Message from "../components/Message";
 import { db, auth } from "../firebase";
-import { doc ,updateDoc ,collection, query, where, onSnapshot, addDoc,Timestamp,orderBy} from "firebase/firestore";
+import {
+  doc,
+  collection,
+  query,
+  where,
+  onSnapshot,
+  addDoc,
+  Timestamp,
+  orderBy,
+  setDoc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
   const [chat, setChat] = useState("");
-  const [text, setText]= useState("");
-  const [msgs,setMsgs] = useState([])
-  const user1 = auth.currentUser.uid
-  // useEffect( () => () => {
-  //   async function handleOffline() {
-  //     await updateDoc(doc(db, "users", auth.currentUser.uid), {
-  //       isOnline: false,
-  //     });
-      
-  //   }
-  //   handleOffline()
+  const [text, setText] = useState("");
+  const [msgs, setMsgs] = useState([]);
+  const user1 = auth.currentUser.uid;
+
+  //   useEffect( () => () => {
+  //     async function handleOffline() {
+  //       await updateDoc(doc(db, "users", auth.currentUser.uid), {
+  //         isOnline: false,
+  //       });
+
+  //     }
+  //     handleOffline()
   // }, [] );
 
-  // useEffect( () => {
-  //   async function handleOnline() {
-  //     await updateDoc(doc(db, "users", auth.currentUser.uid), {
-  //       isOnline: true,
-  //     });
-      
-  //   }
-  //   handleOnline()
-  // }, [] );
+  //   // useEffect( () => {
+  //   //   async function handleOnline() {
+  //   //     await updateDoc(doc(db, "users", auth.currentUser.uid), {
+  //   //       isOnline: true,
+  //   //     });
+
+  //   //   }
+  //   //   handleOnline()
+  //   // }, [] );
 
   useEffect(() => {
+
     const usersRef = collection(db, "users");
     // create query object
     const q = query(usersRef, where("uid", "not-in", [user1]));
     // execute query
     const unsub = onSnapshot(q, (querySnapshot) => {
+      setUsers([]);
       querySnapshot.forEach((doc) => {
         setUsers((current) => [...current, doc.data()]);
       });
     });
     return () => unsub();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const selectUser = (user) => {
+  const selectUser = async (user) => {
     setChat(user);
-    const user2 = user.uid
-    const id = user1 > user2 ? `${user1+user2}`:`${user2+user1}`
-    const msgsRef = collection(db, "messages",id,"chat")
-    const q =query(msgsRef, orderBy("createdAt","asc"))
+    const user2 = user.uid;
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+    const msgsRef = collection(db, "messages", id, "chat");
+    const q = query(msgsRef, orderBy("createdAt", "asc"));
 
-    onSnapshot(q,querySnapshot=>{
-      let msgs =[]
-      querySnapshot.forEach(doc =>{
-        msgs.push(doc.data())
-      })
+    onSnapshot(q, (querySnapshot) => {
+      let msgs = [];
+      querySnapshot.forEach((doc) => {
+        msgs.push(doc.data());
+      });
       setMsgs(msgs);
-    })
+    });
+    // get last message from logged in user and selected user
+    const docSnap = await getDoc(doc(db, "lastMsg", id));
+    if (docSnap.data() && docSnap.data().from !== user1) {
+      await updateDoc(doc(db, "lastMsg", id), { unread: false });
+    }
   };
-  const handleSubmit = async (e)=>{
-    e.preventDefault()
-    const user2 = chat.uid
-    const id = user1 > user2 ? `${user1+user2}`:`${user2+user1}`
-    await addDoc(collection(db,"messages",id,"chat"),{
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user2 = chat.uid;
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+    await addDoc(collection(db, "messages", id, "chat"), {
       text,
       from: user1,
       to: user2,
-      createdAt: Timestamp.fromDate(new Date())
-    })
+      createdAt: Timestamp.fromDate(new Date()),
+    });
+    await setDoc(doc(db, "lastMsg", id), {
+      text,
+      from: user1,
+      to: user2,
+      createdAt: Timestamp.fromDate(new Date()),
+      unread: true,
+    });
     setText("");
-  }
+  };
 
   return (
     <div className="home_container">
       <div className="users_container">
         {users.map((user) => (
-          <User key={user.uid} user={user} selectUser={selectUser} />
+          <User
+            key={user.uid}
+            user={user}
+            selectUser={selectUser}
+            user1={user1}
+            chat={chat}
+          />
         ))}
       </div>
       <div className="message_container">
@@ -86,10 +121,18 @@ export default function Home() {
               <h3> {chat.name}</h3>
             </div>
             <div className="messages">
-              {msgs.length ? msgs.map((msg,i)=><Message key={i} msg={msg} user1={user1} />) : null}
+              {msgs.length
+                ? msgs.map((msg, i) => (
+                    <Message key={i} msg={msg} user1={user1} />
+                  ))
+                : null}
             </div>
             <div>
-            <MessageForm handleSubmit={handleSubmit} text={text} setText={setText} />
+              <MessageForm
+                handleSubmit={handleSubmit}
+                text={text}
+                setText={setText}
+              />
             </div>
           </>
         ) : (
